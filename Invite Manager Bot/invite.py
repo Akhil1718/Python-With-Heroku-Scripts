c5f928997e19f2c232067ebc54957265140198e9
Akhil1718 <61012661+Akhil1718@users.noreply.github.com>
2020-03-19 01:21:00 +0530

    Update invite.py

--- Invite Manager Bot/invite.py
+++ Invite Manager Bot/invite.py
@@ -37,7 +37,7 @@ async def on_member_join(new_member):
 
 @client.event
 async def on_message(message):
-	if message.content=='TZ!invites': '''Here is the command section'''
+	if message.content=='a!invites': '''Here is the command section'''
 		total_uses=0
 		embed=discord.Embed(title='__Invites from {}__'.format(message.author.name))
 		invites = await client.invites_from(message.server)
